a5eb796cc46db778eabd76d00b41a46cc8bcd002
samson <samsonmendonca96@gmail.com>
2024-02-10 22:11:28 +0530

    api updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,6 +10,8 @@ export const excersiseOptions=  {
 export const YouTubeV3options= {
   method: 'GET',
   headers: {
+
+
     'X-RapidAPI-Key': '5dc4379b64msh935c2e8c9fa4072p145927jsn4e7aceb35b90',
     'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
   }
